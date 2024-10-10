5025183d37a93bec1e8f0241f4235a9c44c493b9
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2024-10-10 13:04:32 +0200

    chore: Open Datenschutzerklärung in target _blank.

--- digital_meal/tool/forms.py
+++ digital_meal/tool/forms.py
@@ -88,7 +88,7 @@ class ClassroomCreateForm(forms.ModelForm):
         help_texts = {
             'agb_agree': 'Ich bin damit einverstanden, dass ich dieses Modul nur mit Schüler:innen verwende, '
                          'die mindestens 14 Jahre alt sind. Zudem bestätige ich, dass ich die '
-                         '<a href="/tool/datenschutz">Datenschutzerklärung</a> gelesen habe und damit einverstanden bin.'
+                         '<a href="/tool/datenschutz" target="_blank">Datenschutzerklärung</a> gelesen habe und damit einverstanden bin.'
         }
 
     def clean(self):
