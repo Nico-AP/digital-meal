8a89244e67f5c7893b84b0ac5d75954e4a6e5f22
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-02-04 10:17:36 +0100

    refactor: Improve pep8 compliance.

--- digital_meal/endpoints/apis.py
+++ digital_meal/endpoints/apis.py
@@ -9,11 +9,13 @@ from django.views.decorators.debug import sensitive_variables
 
 from ddm.models.auth import ProjectTokenAuthenticator
 from ddm.models.core import (
-    Participant, DonationBlueprint, DataDonation, DonationProject, QuestionnaireResponse
+    Participant, DonationBlueprint, DataDonation, DonationProject,
+    QuestionnaireResponse
 )
 from ddm.models.encryption import Decryption
 from ddm.models.serializers import (
-    ProjectSerializer, SerializerDecryptionMixin, DonationSerializer, ResponseSerializer
+    ProjectSerializer, SerializerDecryptionMixin, DonationSerializer,
+    ResponseSerializer
 )
 from ddm.views.apis import DDMAPIMixin, user_is_allowed
 
@@ -23,13 +25,20 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-class DonationSerializerAlt(SerializerDecryptionMixin, serializers.HyperlinkedModelSerializer):
+class DonationSerializerAlt(SerializerDecryptionMixin,
+                            serializers.HyperlinkedModelSerializer):
     project = serializers.IntegerField(source='project.id')
     participant = serializers.IntegerField(source='participant.id')
 
     class Meta:
         model = DataDonation
-        fields = ['time_submitted', 'consent', 'status', 'project', 'participant']
+        fields = [
+            'time_submitted',
+            'consent',
+            'status',
+            'project',
+            'participant'
+        ]
 
 
 class DDMBaseProjectApi(APIView, DDMAPIMixin):
@@ -52,7 +61,7 @@ class DDMBaseProjectApi(APIView, DDMAPIMixin):
         return response
 
     def get_project(self):
-        """ Returns project instance. """
+        """ Returns project instance based on request parameter. """
         return DonationProject.objects.filter(pk=self.kwargs['pk']).first()
 
     def check_request_allowed(self, request, project):
@@ -74,7 +83,10 @@ class DDMBaseProjectApi(APIView, DDMAPIMixin):
     def get_secret(self, request, project):
         secret = project.secret_key
         if project.super_secret:
-            super_secret = None if 'Super-Secret' not in request.headers else request.headers['Super-Secret']
+            if 'Super-Secret' not in request.headers:
+                super_secret = None
+            else:
+                super_secret = request.headers['Super-Secret']
             if super_secret is None:
                 self.create_event_log(
                     descr='Failed Attempt',
@@ -86,7 +98,7 @@ class DDMBaseProjectApi(APIView, DDMAPIMixin):
         return secret
 
     def create_zip_response(self, data):
-        """ Creates an HttpResponse object containing the provided zip file. """
+        """ Creates an HttpResponse object containing the created zip file. """
         zip_in_mem = self.create_zip(data)
         response = HttpResponse(zip_in_mem, content_type='application/zip')
         response['Content-Length'] = len(zip_in_mem)
@@ -97,9 +109,16 @@ class DDMBaseProjectApi(APIView, DDMAPIMixin):
     def create_zip(content, filename='data.json'):
         """ Creates a zip file in memory. """
         buffer = io.BytesIO()
-        with zipfile.ZipFile(buffer, mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
+        with zipfile.ZipFile(buffer, mode='w',
+                             compression=zipfile.ZIP_DEFLATED) as zf:
             with zf.open(filename, 'w') as json_file:
-                json_file.write(json.dumps(content, ensure_ascii=False, separators=(',', ':')).encode('utf-8'))
+                json_file.write(
+                    json.dumps(
+                        content,
+                        ensure_ascii=False,
+                        separators=(',', ':')
+                    ).encode('utf-8')
+                )
                 zf.testzip()
         zip_in_memory = buffer.getvalue()
         buffer.flush()
@@ -108,15 +127,30 @@ class DDMBaseProjectApi(APIView, DDMAPIMixin):
 
 class ClassOverviewAPI(DDMBaseProjectApi):
     """
-    Gather data for Classroom overview.
+    API endpoint to gather classroom statistics overview for a project.
+
+    This API collects and returns the following data in a dictionary:
+    - n_donations: The number of donations collected in the class for each
+        blueprint,
+    - n_finished: The number of participants who have completed the
+        donation process,
+    - n_not_finished: The number of participants who have not completed the
+        donation process,
+    - donation_dates: The dates when the donations collected in the class
+        were uploaded.
 
     Expected parameters:
-    - 'class': external classroom id
-    """
+    - 'class': External classroom ID (required)
 
+    Raises:
+        BadRequest: If the 'class' parameter is not provided.
+    """
     def create_response(self, data, **kwargs):
         response = Response(json.dumps(data))
         return response
+
+    def get_formatted_date_list(self, date_list):
+        return [d.strftime('%Y-%m-%dT%H:%M:%S.%fZ') for d in set(date_list)]
 
     def get_data(self, project, secret):
         classroom_id = self.request.query_params.get('class')
@@ -135,38 +169,42 @@ class ClassOverviewAPI(DDMBaseProjectApi):
         blueprints = DonationBlueprint.objects.filter(project=project)
         for blueprint in blueprints:
             blueprint_donations = blueprint.datadonation_set.filter(
-                participant__pk__in=participant_ids, status='success').defer('data')
+                participant__pk__in=participant_ids,
+                status='success'
+            ).defer('data')
 
             n_donations[blueprint.name] = len(blueprint_donations)
-            donation_dates.extend(blueprint_donations.values_list('time_submitted', flat=True))
+            donation_dates.extend(
+                blueprint_donations.values_list('time_submitted', flat=True))
 
         data = {
             'n_donations': n_donations,
-            'n_not_finished': (n_started - n_finished),
             'n_finished': n_finished,
-            'donation_dates': [d.strftime('%Y-%m-%dT%H:%M:%S.%fZ') for d in list(set(donation_dates))]
+            'n_not_finished': (n_started - n_finished),
+            'donation_dates': self.get_formatted_date_list(donation_dates)
         }
         return data
 
 
 class ClassReportAPI(DDMBaseProjectApi):
     """
-    Gather data for Classroom report including data of all participants
-    belonging to the classroom.
+    API endpoint to gather the data required to generate a classroom report.
+
+    The data includes the donations of all participants belonging to the
+    classroom if at least 5 donations exist. Otherwise, it returns None for
+    the donations.
 
     Expected parameters:
-    - 'class': external classroom id
+    - 'class': External classroom ID (required)
 
-    Notes:
-        - Only returns data if data of at least 5 persons is available for a classroom.
+    Raises:
+        BadRequest: If the 'class' parameter is not provided.
     """
-
     def create_response(self, data, **kwargs):
         response = Response(json.dumps(data))
         return response
 
     def get_data(self, project, secret):
-        # blueprints = DonationBlueprint.objects.filter(project=project)
         classroom_id = self.request.query_params.get('class')
         if not classroom_id:
             raise BadRequest('No classroom id provided')
@@ -179,14 +217,17 @@ class ClassReportAPI(DDMBaseProjectApi):
 
         try:
             decryptor = Decryption(secret, project.get_salt())
-            for blueprint in blueprints:
-                blueprint_donations = blueprint.datadonation_set.filter(
+            for bp in blueprints:
+                bp_donations = bp.datadonation_set.filter(
                     participant__pk__in=participants, status='success')
-                if len(blueprint_donations) < 5:
-                    donations[blueprint.name] = None
+                if len(bp_donations) < 5:
+                    donations[bp.name] = None
                 else:
-                    donations[blueprint.name] = [DonationSerializer(
-                        d, decryptor=decryptor).data for d in blueprint_donations]
+                    donations[bp.name] = [
+                        DonationSerializer(d, decryptor=decryptor).data
+                        for d
+                        in bp_donations
+                    ]
         except ValueError:
             self.create_event_log(
                 descr='Failed Download Attempt',
@@ -203,7 +244,7 @@ class ClassReportAPI(DDMBaseProjectApi):
 
 class IndividualReportAPI(DDMBaseProjectApi):
     """
-    Gather data for an individual report related to one participant.
+    API endpoint to gather the data required to generate an individual report.
 
     Expected parameters:
     - 'participant_id': external participant id
@@ -213,14 +254,16 @@ class IndividualReportAPI(DDMBaseProjectApi):
         response = Response(json.dumps(data))
         return response
 
-    def get_data(self, project, secret):
+    def get_participant(self):
         participant_id = self.request.query_params.get('participant_id')
-        participant = Participant.objects.filter(
-            project=project,
+        return Participant.objects.filter(
+            project=self.project,
             external_id=participant_id
         ).first()
 
-        self.check_participant_allowed(participant)  # Raises an exception if not allowed.
+    def get_data(self, project, secret):
+        participant_id = self.get_participant()
+        self.check_participant_allowed(participant_id)
 
         blueprints = DonationBlueprint.objects.filter(project=project)
         responses = QuestionnaireResponse.objects.filter(
@@ -232,16 +275,20 @@ class IndividualReportAPI(DDMBaseProjectApi):
         try:
             decryptor = Decryption(secret, project.get_salt())
 
-            for blueprint in blueprints:
-                blueprint_donation = blueprint.datadonation_set.filter(
-                    participant__external_id=participant_id, status='success').first()
-                if blueprint_donation:
-                    donations[blueprint.name] = DonationSerializer(
-                        blueprint_donation, decryptor=decryptor).data
+            for bp in blueprints:
+                bp_donation = bp.datadonation_set.filter(
+                    participant__external_id=participant_id,
+                    status='success'
+                ).first()
+                if bp_donation:
+                    donations[bp.name] = DonationSerializer(
+                        bp_donation, decryptor=decryptor).data
 
             if responses:
-                responses = [ResponseSerializer(r, decryptor=decryptor).data['responses']
-                             for r in responses]
+                responses = [
+                    ResponseSerializer(r, decryptor=decryptor).data['responses']
+                    for r in responses
+                ]
 
         except ValueError:
             self.create_event_log(
@@ -265,7 +312,9 @@ class IndividualReportAPI(DDMBaseProjectApi):
         if not participant.completed:
             raise NotFound
 
-        if not (participant.end_time + timezone.timedelta(days=90)) > timezone.now():
+        if not (
+            participant.end_time + timezone.timedelta(days=90)
+        ) > timezone.now():
             raise PermissionDenied
 
         return True
