d045992b3e24af480aefa64de028c28e35f2d5fe
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-02-22 10:30:21 +0100

    chore: Remove api endpoints url.

--- digital_meal/urls.py
+++ digital_meal/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('reports/', include('digital_meal.reports.urls')),
     path('tool/', include('digital_meal.tool.urls')),
     path('', include('digital_meal.website.urls')),
-    path('api/', include('digital_meal.endpoints.urls')),
 ]
