d31a6d7ecce63ee5d70f773bd9542354088b3e0f
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-03-17 07:29:09 +0100

    chore: Prepare management command to delete donations by participants without data.

--- digital_meal/tool/management/commands/clean_participants.py
+++ digital_meal/tool/management/commands/clean_participants.py
@@ -1,12 +1,15 @@
 from datetime import timedelta
+
+from ddm.datadonation.models import DataDonation
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from ddm.participation.models import Participant
 
 
+# TODO: Complete function once the questionnaire has been defined.
 class CleanParticipants(BaseCommand):
     help = (
-        'Deletes participants that have started participation over 180 days '
+        'Deletes donations of participants that have started participation over 180 days '
         'ago and did not explicitly consent to donating their data for '
         'academic research.'
     )
@@ -15,9 +18,21 @@ class CleanParticipants(BaseCommand):
         ref_datetime = timezone.now() - timedelta(days=180)
         participants_to_delete = Participant.objects.filter(
             start_time__lt=ref_datetime,
-            # TODO: Check for donation consent.
+            # TODO: More efficient filtering (could also directly filter based on responses).
         )
-        n_participants = participants_to_delete.count()
-        participants_to_delete.delete()
 
-        self.stdout.write(self.style.SUCCESS(f'Deleted {n_participants}'))
+        # Get questionnaire responses
+        participants_wo_consent = 0
+        responses = []  # TODO: Get responses queryset.
+        for response in responses:
+            response_data = None  # response.get_decrypted_data(project.secret, project.get_salt())
+            consent = response_data.get('consent_variable', True)
+            if consent:
+                continue
+            participants_wo_consent += 1
+
+            participant = response.participant
+            donations = DataDonation.objects.filter(participant=participant)
+            donations.delete()
+
+        self.stdout.write(self.style.SUCCESS(f'Deleted responses of {len(participants_wo_consent)} participants.'))
