e2274de0217e20c0392a1de90f3e7e982935e950
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-03-20 23:45:22 +0100

    fix: Add missing ckeditor default settings.

--- config/settings/base.py
+++ config/settings/base.py
@@ -298,6 +298,109 @@ CKEDITOR_5_CONFIGS = {
             'displayCharacters': False,
             'displayWords': False,
         }
+    },
+    'default': {
+        'blockToolbar': [
+            'paragraph', 'heading1', 'heading2', 'heading3',
+            '|',
+            'bulletedList', 'numberedList',
+            '|',
+            'blockQuote',
+        ],
+        'toolbar': [
+            'heading', '|',
+            'alignment', 'outdent', 'indent', '|',
+            'bold', 'italic', 'underline', 'link', 'highlight', '|',
+            {
+                'label': 'Fonts',
+                'icon': 'text',
+                'items': ['fontSize', 'fontFamily', 'fontColor']
+            }, '|',
+            'bulletedList', 'numberedList', 'insertTable', 'blockQuote', 'code', 'removeFormat', '|',
+            'insertImage', 'fileUpload', 'mediaEmbed', '|',
+            'sourceEditing'
+        ],
+        'image': {
+            'toolbar': ['imageTextAlternative', '|', 'imageStyle:alignLeft',
+                        'imageStyle:alignRight', 'imageStyle:alignCenter', 'imageStyle:side', '|'],
+            'styles': [
+                'full',
+                'side',
+                'alignLeft',
+                'alignRight',
+                'alignCenter',
+            ]
+        },
+        'table': {
+            'contentToolbar': ['tableColumn', 'tableRow', 'mergeTableCells',
+                               'tableProperties', 'tableCellProperties'],
+        },
+        'heading': {
+            'options': [
+                {'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph'},
+                {'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1'},
+                {'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2'},
+                {'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3'}
+            ]
+        },
+        'htmlSupport': {
+            'allow': [
+                {
+                    'name': 'video',
+                    'attributes': {
+                        'height': True,
+                        'width': True,
+                        'controls': True,
+                    },
+                    'styles': True
+                },
+                {
+                    'name': 'p',
+                    'attributes': ATTRIBUTES_TO_ALLOW
+                },
+                {
+                    'name': 'span',
+                    'attributes': ATTRIBUTES_TO_ALLOW
+                },
+                {
+                    'name': 'div',
+                    'attributes': ATTRIBUTES_TO_ALLOW
+                },
+                {
+                    'name': 'a',
+                    'attributes': ATTRIBUTES_TO_ALLOW
+                },
+                {
+                    'name': 'table',
+                    'attributes': ATTRIBUTES_TO_ALLOW
+                },
+                {
+                    'name': 'td',
+                    'attributes': ATTRIBUTES_TO_ALLOW
+                },
+                {
+                    'name': 'th',
+                    'attributes': ATTRIBUTES_TO_ALLOW
+                },
+                {
+                    'name': 'button',
+                    'attributes': ATTRIBUTES_TO_ALLOW
+                },
+                {
+                    'name': 'h1',
+                    'attributes': ATTRIBUTES_TO_ALLOW
+                },
+                {
+                    'name': 'h2',
+                    'attributes': ATTRIBUTES_TO_ALLOW
+                },
+            ],
+            'disallow': []
+        },
+        'wordCount': {
+            'displayCharacters': False,
+            'displayWords': False,
+        }
     }
 }
 
