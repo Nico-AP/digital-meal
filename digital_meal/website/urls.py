99e13b1904222a504844f8f1f5896d29679bc9cf
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-06-24 19:33:11 +0200

    chore: Add tiktok dev verification file.

--- digital_meal/website/urls.py
+++ digital_meal/website/urls.py
@@ -12,8 +12,20 @@ urlpatterns = [
     path('cms/', include('wagtail.admin.urls')),
     path('documents/', include('wagtail.documents.urls')),
     path('newsletter/', NewsletterSignupPage.as_view(), name='newsletter'),
-    path('robots.txt', TemplateView.as_view(template_name='website/robots.txt', content_type='text/plain')),
+    path(
+        'robots.txt',
+        TemplateView.as_view(template_name='website/robots.txt', content_type='text/plain')
+    ),
     path('sitemap.xml', sitemap),
-    path('google2b303e6246e0b3a0.html', TemplateView.as_view(template_name='website/google_search_console_verification.html'), name='google_sc_verification'),
+    path(
+        'google2b303e6246e0b3a0.html',
+        TemplateView.as_view(template_name='website/google_search_console_verification.html'),
+        name='google_sc_verification'
+    ),
+    path(
+        'tiktokYbzF9wdz4BV0HO6cnUyVPchkKCRyEggr.txt',
+        TemplateView.as_view(template_name='website/tiktokYbzF9wdz4BV0HO6cnUyVPchkKCRyEggr.txt'),
+        name='tiktok_dev_verification'
+    ),
     path('', include('wagtail.urls')),
 ]
