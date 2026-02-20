9f9dd5937145fae075271278f8bf41a92f15ee5d
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2026-02-20 13:38:35 +0100

    feat: added tiktok connect page to my dm.

--- mydigitalmeal/datadonation/urls.py
+++ mydigitalmeal/datadonation/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
 import mydigitalmeal.datadonation.views.ddm as ddm_views
+import mydigitalmeal.datadonation.views.portability as port_views
 
 app_name = "datadonation"
 urlpatterns = [
     path(
-        "donation-ddm/",
+        "get-data/",
         ddm_views.DonationViewDDM.as_view(),
         name="datadonation_ddm",
+    ),
+    path(
+        "connect/",
+        port_views.PortabilityEntryView.as_view(),
+        name="port_tt_connect",
     ),
 ]
