34db11f642526a8ed55bf2c3bd208e1f1ce0c73d
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2026-02-27 17:10:00 +0100

    fix: updated db options to use mysql specifics.

--- config/settings/ci.py
+++ config/settings/ci.py
@@ -10,7 +10,7 @@ SECURE_HSTS_PRELOAD = False
 SECURE_HSTS_INCLUDE_SUBDOMAINS = False
 SECURE_PROXY_SSL_HEADER = None
 
-DATABASES["default"]["OPTIONS"] = {"sslmode": "disable"}
+DATABASES["default"]["OPTIONS"] = {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"}
 
 # Speed up password hashing in tests
 PASSWORD_HASHERS = [
