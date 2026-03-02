5aaa8983c00e77e29711ed9dfd32e0ef6682dae0
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2026-03-02 20:18:16 +0100

    fix: typo in url name.

--- mydigitalmeal/infopages/urls.py
+++ mydigitalmeal/infopages/urls.py
@@ -6,7 +6,7 @@ app_name = "infopages"
 urlpatterns = [
     path("about/", views.AboutView.as_view(), name="about"),
     path("info/", views.BackgroundView.as_view(), name="background"),
-    path("dataprotection/", views.DataProtectionView.as_view(), name="dataprotection"),
+    path("dataprotection/", views.DataProtectionView.as_view(), name="data_protection"),
     path("tos/", views.TermsOfServiceView.as_view(), name="tos"),
     path("dps/", views.DataProtectionStatementView.as_view(), name="dps"),
 ]
