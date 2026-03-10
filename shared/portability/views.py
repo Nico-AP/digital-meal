5465d7b6b4e668d552f93bacebff446fff9cc1f6
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2026-03-10 10:22:56 +0100

    fix: corrected viewname of datadonation await view.

--- shared/portability/views.py
+++ shared/portability/views.py
@@ -413,7 +413,7 @@ class TikTokCallbackView(ManageAccessTokenMixin, PortabilitySessionMixin, View):
     def redirect_success(self):
         port_context = self.port_session.get_context()
         if port_context == PortabilityContexts.MY_DM:
-            return redirect("mdm:datadonation:port_tt_await_data")
+            return redirect("mdm:userflow:datadonation:port_tt_await_data")
         else:
             return redirect("tiktok_await_data_download")
 
