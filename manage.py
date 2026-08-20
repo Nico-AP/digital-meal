13f14605533f48986d9655cd751831f485196d26
Nico-AP <pfiffner.nico@proton.me>
2026-08-20 09:52:27 +0200

    chore: change default settings to production

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 
 import os
@@ -12,7 +11,7 @@ env.read_env()
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
     try:
         from django.core.management import execute_from_command_line  # noqa: PLC0415
     except ImportError as exc:
